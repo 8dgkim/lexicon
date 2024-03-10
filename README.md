7472f3832b2ab80e695591ff13e91510d7b82307
8dgkim <0dgkim@gmail.com>
2024-03-10 18:37:47 -0400

    added template; adjusted iframe height

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ source :
 ## log
 - 2024-02-04 :: initiated; added Russian; deployed
 - 2024-02-05 :: added French
+- 2024-03-10 :: added template; adjusted iframe for phone screen
---
+++ languages/template/template.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const lexiconList = document.getElementById("lexicon-list");
+    const wiktionaryFrame = document.getElementById("wiktionary-frame");
+
+    fetch("template.json")
+        .then(response => response.json())
+        .then(data => renderLexicon(data))
+        .catch(error => console.error('Error fetching lexicon:', error));
+
+    function renderLexicon(languageLexicon) {
+        lexiconList.innerHTML = "";
+        languageLexicon.forEach((entry) => {
+            const listItem = document.createElement("div");
+            listItem.innerHTML = `<a href="#" onclick="showWord('${entry.link}')">${entry.word}</a>`;
+            lexiconList.appendChild(listItem);
+        });
+    }
+
+    window.showWord = function (wiktionaryLink) {
+        wiktionaryFrame.src = wiktionaryLink;
+    };
+});
